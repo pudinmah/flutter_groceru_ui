60f29e302303674c14b89537b5ea81d161aefb9a
pudinmah <pudinmah69@gmail.com>
2024-06-14 05:06:10 +0700

    create AppBar

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: const Icon(
+          Icons.menu,
+          color: Colors.black,
+        ),
+        title: Center(
+          child: const Text(
+            'Toko Buah & Sayur',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        actions: [
+          Row(
+            children: [
+              const Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              Stack(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.shopping_cart,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    right: 3,
+                    child: Container(
+                      height: 20,
+                      width: 20,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle, color: Colors.red),
+                      child: const Center(
+                        child: Text(
+                          '2',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w800),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
