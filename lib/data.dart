82357dde7861b348c875e92164862ff50f9c6c5f
pudinmah <pudinmah69@gmail.com>
2024-06-14 05:10:37 +0700

    class Model

---
+++ lib/data.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String name;
+  final String price;
+  final String quantity;
+  final String image;
+  final String description;
+  
+  Product(
+    this.name,
+    this.price,
+    this.quantity,
+    this.image,
+    this.description,
+  );
+}
