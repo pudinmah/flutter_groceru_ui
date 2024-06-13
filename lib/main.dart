6cbda16fe04484ae34c740d2d541d453e109bcd5
pudinmah <pudinmah69@gmail.com>
2024-06-14 06:43:45 +0700

    Hero widget

--- lib/main.dart
+++ lib/main.dart
@@ -36,8 +36,8 @@ class _MainPageState extends State<MainPage> {
           Icons.menu,
           color: Colors.black,
         ),
-        title: Center(
-          child: const Text(
+        title: const Center(
+          child: Text(
             'Toko Buah & Sayur',
             style: TextStyle(color: Colors.black),
           ),
@@ -84,7 +84,10 @@ class _MainPageState extends State<MainPage> {
       body: Container(
         padding: const EdgeInsets.all(10),
         child: GridView.count(
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
           crossAxisCount: 2,
+          childAspectRatio: 0.6,
           children: [
             Card(
               elevation: 2,
@@ -92,8 +95,80 @@ class _MainPageState extends State<MainPage> {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20),
               ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: data.image,
+                    child: SizedBox(
+                      width: 150,
+                      child: Image.asset(data.image),
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  Text(
+                    'Rp ${data.price}',
+                    style: const TextStyle(
+                        color: Colors.green,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    data.name,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(height: 2),
+                  Text(
+                    data.quantity,
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.w300),
+                  ),
+                  const SizedBox(height: 4),
+                  const Divider(
+                    height: 2,
+                    color: Colors.grey,
+                  ),
+                  const SizedBox(height: 10),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.add_shopping_cart,
+                            size: 20,
+                            color: Colors.green,
+                          ),
+                          SizedBox(width: 2),
+                          Text(
+                            'Beli',
+                            style: TextStyle(color: Colors.green, fontSize: 16),
+                          )
+                        ],
+                      ),
+                      SizedBox(width: 8),
+                      Row(
+                        children: [
+                          Icon(Icons.remove_circle_outline,
+                              size: 18, color: Colors.green),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 5),
+                            child: Text('0'),
+                          ),
+                          Icon(
+                            Icons.add_circle_outline,
+                            size: 18,
+                            color: Colors.green,
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
-            Card(),
+            const Card(),
           ],
         ),
       ),
