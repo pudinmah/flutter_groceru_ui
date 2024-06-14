89987d15a917d28cebaafe7fea779c475f96cfac
pudinmah <pudinmah69@gmail.com>
2024-06-14 07:01:40 +0700

    Gridview builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_grocery_ui/data.dart';
+import 'package:flutter_grocery_ui/item_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -83,93 +84,17 @@ class _MainPageState extends State<MainPage> {
       ),
       body: Container(
         padding: const EdgeInsets.all(10),
-        child: GridView.count(
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10,
-          crossAxisCount: 2,
-          childAspectRatio: 0.6,
-          children: [
-            Card(
-              elevation: 2,
-              shadowColor: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Hero(
-                    tag: data.image,
-                    child: SizedBox(
-                      width: 150,
-                      child: Image.asset(data.image),
-                    ),
-                  ),
-                  const SizedBox(height: 6),
-                  Text(
-                    'Rp ${data.price}',
-                    style: const TextStyle(
-                        color: Colors.green,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  Text(
-                    data.name,
-                    style: const TextStyle(
-                        fontSize: 20, fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(height: 2),
-                  Text(
-                    data.quantity,
-                    style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.w300),
-                  ),
-                  const SizedBox(height: 4),
-                  const Divider(
-                    height: 2,
-                    color: Colors.grey,
-                  ),
-                  const SizedBox(height: 10),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.add_shopping_cart,
-                            size: 20,
-                            color: Colors.green,
-                          ),
-                          SizedBox(width: 2),
-                          Text(
-                            'Beli',
-                            style: TextStyle(color: Colors.green, fontSize: 16),
-                          )
-                        ],
-                      ),
-                      SizedBox(width: 8),
-                      Row(
-                        children: [
-                          Icon(Icons.remove_circle_outline,
-                              size: 18, color: Colors.green),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 5),
-                            child: Text('0'),
-                          ),
-                          Icon(
-                            Icons.add_circle_outline,
-                            size: 18,
-                            color: Colors.green,
-                          )
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-            const Card(),
-          ],
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            crossAxisCount: 2,
+            childAspectRatio: 0.6,
+          ),
+          itemBuilder: (context, index) {
+            return ItemWidget(product: allData[index]);
+          },
+          itemCount: allData.length,
         ),
       ),
     );
